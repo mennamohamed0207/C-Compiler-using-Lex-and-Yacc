4a342dd80a35b4357dcd7988f2d0c53e86e08cb5
mennamohamed0207 <menamohamed0207@gmail.com>
2025-04-16 09:57:39 +0200

    modifying run_tests script and update readme

--- test_expr.c
+++ test_expr.c
@@ -1,6 +1,11 @@
-INT CALC() {
-    INT A = (5 + 3) * 2;
-    INT B = A >= 10 && A < 20;
-    INT C = B == 1 || NOT(B);
-    RETURN C;
+int main() {
+    int a = 5 * (3 + 2);
+    int b = (a > 10) && (a < 30);
+    int c = b || (a % 2 == 0);
+    
+    int x = 10;
+    x += 5;
+    x *= 2;
+    
+    return 0;
 }
