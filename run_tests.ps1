276600b94a5c1750d2a8d69c9ef5b61d10dc8dfd
mennamohamed0207 <menamohamed0207@gmail.com>
2025-04-16 10:01:50 +0200

    modifying folder structure

--- run_tests.ps1
+++ run_tests.ps1
@@ -5,30 +5,38 @@ gcc parser.tab.c lex.yy.c -o compiler
 
 
 Write-Host "-----------------------------------Running test_switch.c-----------------------------------"
-Get-Content test_switch.c | .\compiler
+Get-Content testcases\test_switch.c | .\compiler
 
 Write-Host "`n-----------------------------------Running test_valid.c-----------------------------------"
-Get-Content test_valid.c | .\compiler 
+Get-Content testcases\test_valid.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_types.c-----------------------------------"
-Get-Content test_types.c | .\compiler
+Get-Content testcases\test_types.c | .\compiler
 
 Write-Host "`n-----------------------------------Running test_error.c-----------------------------------"
-Get-Content test_error.c | .\compiler 
+Get-Content testcases\test_error.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_expression.c-----------------------------------"
-Get-Content test_expr.c | .\compiler 
+Get-Content testcases\test_expr.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_if.c-----------------------------------"
-Get-Content test_if.c | .\compiler 
+Get-Content testcases\test_if.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_while.c-----------------------------------"
-Get-Content test_while.c | .\compiler 
+Get-Content testcases\test_while.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_for.c-----------------------------------"
-Get-Content test_for.c | .\compiler 
+Get-Content testcases\test_for.c | .\compiler 
 
 Write-Host "`n-----------------------------------Running test_dowhile.c-----------------------------------"
-Get-Content test_dowhile.c | .\compiler 
+Get-Content testcases\test_dowhile.c | .\compiler 
+
+Write-Host "`n-----------------------------------Running test_function.c-----------------------------------"
+Get-Content testcases\test_function.c | .\compiler 
+
+
+
+
+
 
 
